373a1c1989bef7f447bec56c1ade82ae486226f2
b1tg <b1tg@users.noreply.github.com>
2021-10-09 23:28:48 +0800

    Auto adjust suffix

--- src/lib.rs
+++ src/lib.rs
@@ -23,6 +23,10 @@ pub fn format_oxford_gb(abc: String) -> String {
     let abc = abc.replace(" 3 ", "\n3. ");
     return abc;
 }
+pub fn adjust_word_suffix_iter(input: &str) -> impl Iterator<Item = String> {
+    let word_list = adjust_word_suffix(input);
+    word_list.into_iter()
+}
 
 pub fn adjust_word_suffix(input: &str) -> Vec<String> {
     // doubled => double
@@ -35,14 +39,17 @@ pub fn adjust_word_suffix(input: &str) -> Vec<String> {
     // t => tion
     // biased => bias
     //   => ed
+    // downloads => download
+    //   => s
 
-    let mut word_list = vec![];
+    let mut word_list = vec![input.clone().to_string()];
     let trans_table = &[
         ("tion", "t"),
         ("ies", "y"),
         ("ed", "e"),
         ("ed", ""),
         ("es", "e"),
+        ("s", ""),
     ];
     for (dst, src) in trans_table {
         if input.ends_with(dst) {
@@ -51,6 +58,8 @@ pub fn adjust_word_suffix(input: &str) -> Vec<String> {
         }
     }
 
+    // hack
+    word_list.dedup_by(|a, b| a.eq_ignore_ascii_case(b));
     return word_list;
 }
 
@@ -67,13 +76,25 @@ impl Index {
 
 #[derive(Debug, Clone, Default)]
 pub struct Dict {
-    dict: HashMap<String, Index>,
+    pub name: String,
+    dict: HashMap<String, Index>, // <word, position>
 }
 /// idx file
 ///     word_str: UTF-8 string , terminate by '\0'
 ///     word_data_offset: u32, word data offset in .dict file
 ///     word_data_size:   u32, word data size in .dict file
 impl Dict {
+    // pub fn name(self) -> String {
+    //     self.name.clone()
+    // }
+    pub fn new(name: &str, path: &str) -> Self {
+        let home = std::env::var("HOME").unwrap();
+        let filepath = path.replace("~", &home);
+        let mut dict = Dict::default();
+        dict.name = name.to_string();
+        dict.add_dict_file(&filepath);
+        dict
+    }
     pub fn add_dict_file(&mut self, filepath: &str) {
         let mut content: Vec<u8> = vec![];
         let idx_path = format!("{}.idx", filepath);
@@ -111,7 +132,7 @@ impl Dict {
             idx += 8;
         }
 
-        println!("Total words in dict: {}", self.dict.len());
+        println!("Total words in dict <{}>: {}", self.name, self.dict.len());
     }
     pub fn enter_lookup_loop(&self) {
         loop {
@@ -130,5 +151,13 @@ impl Dict {
             return Some(index.get_data());
         }
         None
+    }
+}
+
+#[test]
+fn test_iter() {
+    let a = adjust_word_suffix_iter("downloads");
+    for i in a {
+        dbg!(&i);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,39 @@
 use colored::*;
 use stardict_rs::*;
-use std::collections::HashMap;
 use std::io;
 use std::io::Write;
 
-fn main() {
-    let mut dictmap: HashMap<String, Dict> = HashMap::new();
+const MAX_RESULTS: u8 = 2;
 
-    let mut dict = Dict::default();
+fn main() {
+    let mut dicts: Vec<Dict> = vec![];
+
+    dicts.push(Dict::new(
+        "langdao-ec-gb",
+        "~/Downloads/stardict-files/stardict-langdao-ec-gb-2.4.2/langdao-ec-gb",
+    ));
+
     // http://download.huzheng.org/zh_CN/stardict-oxford-gb-2.4.2.tar.bz2
-    dict.add_dict_file("/tmp/stardict-oxford-gb-2.4.2/oxford-gb");
-    dictmap.insert("oxford-gb".to_string(), dict);
+    dicts.push(Dict::new(
+        "oxford-gb",
+        "~/Downloads/stardict-files/stardict-oxford-gb-2.4.2/oxford-gb",
+    ));
 
     loop {
         let mut cmd = String::new();
         print!(">> ");
         let _ = io::stdout().flush();
         let _ = io::stdin().read_line(&mut cmd);
-        cmd = cmd.trim().to_string();
-        let mut word_exist = false;
-        loop {
-            for (name, dict) in &dictmap {
-                let res = dict.find_word(&cmd);
+        cmd = cmd.trim().to_string().to_ascii_lowercase();
+        let mut word_found_times = 0;
+        for dict in &dicts {
+            let name = &dict.name;
+            let a = adjust_word_suffix_iter(&cmd);
+            for i in a {
+                let res = dict.find_word(&i);
                 if res.is_some() {
-                    word_exist = true;
-                    let pp = format!("{}=>", name);
+                    word_found_times += 1;
+                    let pp = format!("{} => {}", name, i);
                     println!("{}", pp.bold());
                     if name == "oxford-gb" {
                         println!("{}", format_oxford_gb(res.unwrap()));
@@ -32,18 +41,15 @@ fn main() {
                         println!("{}", res.unwrap());
                     }
                     println!("{}", "==========".bold());
+                    break; // max 1 hit per dictionary
                 }
             }
-            if !word_exist {
-                println!("{} not found", &cmd);
-                if cmd.ends_with("ted") {
-                    cmd = format!("{}te", cmd[..cmd.len() - 3].to_string());
-                } else {
-                    break;
-                }
-            } else {
+            if word_found_times >= MAX_RESULTS {
                 break;
             }
+        }
+        if word_found_times == 0 {
+            println!("{} not found", &cmd);
         }
     }
 }
